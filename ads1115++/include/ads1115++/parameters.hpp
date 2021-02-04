aff6141b1ee67452a706785abbca0bd00283cdff
Lorenz Steinert <lorenz@steinerts.de>
2021-02-04 22:07:56 +0000

    switch to typed enums

--- ads1115++/include/ads1115++/parameters.hpp
+++ ads1115++/include/ads1115++/parameters.hpp
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef ADS1115_PARAMETERS_HPP_
+#define ADS1115_PARAMETERS_HPP_
 
 // stl
 #include <cstdint>
@@ -6,65 +7,69 @@
 
 namespace ADS1115
 {
-    enum class ADDR { GND, VDD, SDA, SCL };
-
-    enum class OS { NON, SINGLE_CONV };
-    enum class MUX {
-        AIN0_AIN1,
-        AIN0_AIN3,
-        AIN1_AIN3,
-        AIN2_AIN3,
-        AIN0_GND,
-        AIN1_GND,
-        AIN2_GND,
-        AIN3_GND
+    enum class ADDR : uint8_t {
+        GND = 0x48,
+        VDD = 0x49,
+        SDA = 0x4a,
+        SCL = 0x4b,
     };
-    enum class PGA { FS_6_144, FS_4_096, FS_2_048, FS_1_024, FS_0_512, FS_0_256 };
-    enum class MODE { CONT_CONV, SINGLE_CONV };
 
-    enum class DR { SPS_8, SPS_16, SPS_32, SPS_64, SPS_128, SPS_250, SPS_475, SPS_860 };
-    enum class COMP_MODE { TRAD_COMP, WINDOW_COMP };
-    enum class COMP_POL { LOW, HIGH };
-    enum class COMP_LAT { NON_LATCHING, LATCHING };
-    enum class COMP_QUE { ONE_CONV, TWO_CONV, FOUR_CONV, DISABLE_COMP };
+    enum class OS : uint16_t {
+        NON = 0x0000,
+        SINGLE_CONV = 0x8000,
+    };
+    enum class MUX : uint16_t {
+        AIN0_AIN1 = 0x0000,
+        AIN0_AIN3 = 0x1000,
+        AIN1_AIN3 = 0x2000,
+        AIN2_AIN3 = 0x3000,
+        AIN0_GND = 0x4000,
+        AIN1_GND = 0x5000,
+        AIN2_GND = 0x6000,
+        AIN3_GND = 0x7000,
+    };
+    enum class PGA : uint16_t {
+        FS_6_144 = 0x0000,
+        FS_4_096 = 0x0200,
+        FS_2_048 = 0x0400,
+        FS_1_024 = 0x0600,
+        FS_0_512 = 0x0800,
+        FS_0_256 = 0x0a00,
+    };
+    enum class MODE : uint16_t {
+        CONT_CONV = 0x0000,
+        SINGLE_CONV = 0x0100,
+    };
 
-    namespace detail
-    {
-        const std::map<ADDR, uint8_t> addr_map { { ADDR::GND, 0x48 },
-                                                 { ADDR::VDD, 0x49 },
-                                                 { ADDR::SDA, 0x4a },
-                                                 { ADDR::SCL, 0x4b } };
+    enum class DR : uint16_t {
+        SPS_8 = 0x0000,
+        SPS_16 = 0x0020,
+        SPS_32 = 0x0040,
+        SPS_64 = 0x0060,
+        SPS_128 = 0x0080,
+        SPS_250 = 0x00a0,
+        SPS_475 = 0x00c0,
+        SPS_860 = 0x00e0,
+    };
+    enum class COMP_MODE : uint16_t {
+        TRAD_COMP = 0x0000,
+        WINDOW_COMP = 0x0010,
+    };
+    enum class COMP_POL : uint16_t {
+        LOW = 0x0000,
+        HIGH = 0x0008,
+    };
+    enum class COMP_LAT : uint16_t {
+        NON_LATCHING = 0x0000,
+        LATCHING = 0x0004,
+    };
+    enum class COMP_QUE : uint16_t {
+        ONE_CONV = 0x0000,
+        TWO_CONV = 0x0001,
+        FOUR_CONV = 0x0002,
+        DISABLE_COMP = 0x0003,
+    };
 
-        const std::map<OS, uint8_t> os_map { { OS::NON, 0x00 }, { OS::SINGLE_CONV, 0x80 } };
-
-        const std::map<MUX, uint8_t> mux_map { { MUX::AIN0_AIN1, 0x00 }, { MUX::AIN0_AIN3, 0x10 },
-                                               { MUX::AIN1_AIN3, 0x20 }, { MUX::AIN2_AIN3, 0x30 },
-                                               { MUX::AIN0_GND, 0x40 },  { MUX::AIN1_GND, 0x50 },
-                                               { MUX::AIN2_GND, 0x60 },  { MUX::AIN3_GND, 0x70 }
-
-        };
-        const std::map<PGA, uint8_t> pga_map {
-            { PGA::FS_6_144, 0x00 }, { PGA::FS_4_096, 0x02 }, { PGA::FS_2_048, 0x04 },
-            { PGA::FS_1_024, 0x06 }, { PGA::FS_0_512, 0x08 }, { PGA::FS_0_256, 0x0a },
-        };
-        const std::map<MODE, uint8_t> mode_map { { MODE::CONT_CONV, 0x00 },
-                                                 { MODE::SINGLE_CONV, 0x01 } };
-
-        const std::map<DR, uint8_t> dr_map {
-            { DR::SPS_8, 0x00 },   { DR::SPS_16, 0x20 },  { DR::SPS_32, 0x40 },
-            { DR::SPS_64, 0x60 },  { DR::SPS_128, 0x80 }, { DR::SPS_250, 0xa0 },
-            { DR::SPS_475, 0xc0 }, { DR::SPS_860, 0xe0 },
-        };
-        const std::map<COMP_MODE, uint8_t> comp_mode_map { { COMP_MODE::TRAD_COMP, 0x00 },
-                                                           { COMP_MODE::WINDOW_COMP, 0x10 } };
-        const std::map<COMP_POL, uint8_t> comp_pol_map { { COMP_POL::LOW, 0x00 },
-                                                         { COMP_POL::HIGH, 0x08 } };
-        const std::map<COMP_LAT, uint8_t> comp_lat_map { { COMP_LAT::NON_LATCHING, 0x00 },
-                                                         { COMP_LAT::LATCHING, 0x04 } };
-        const std::map<COMP_QUE, uint8_t> comp_que_map { { COMP_QUE::ONE_CONV, 0x00 },
-                                                         { COMP_QUE::TWO_CONV, 0x01 },
-                                                         { COMP_QUE::FOUR_CONV, 0x02 },
-                                                         { COMP_QUE::DISABLE_COMP, 0x03 } };
-
-    } // namespace detail
 } // namespace ADS1115
+
+#endif // ADS1115_PARAMETERS_HPP_
