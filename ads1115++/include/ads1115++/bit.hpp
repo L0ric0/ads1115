945eb7e2bbef2aef65d57020b418b12aa48ff7c5
Lorenz Steinert <lorenz@steinerts.de>
2021-02-05 14:29:29 +0000

    add own implementation of bit_cast until gcc 11 is out

---
+++ ads1115++/include/ads1115++/bit.hpp
@@ -0,0 +1,29 @@
+#ifndef ADS1115_BIT_HPP_
+#define ADS1115_BIT_HPP_
+
+// stl
+#include <cstring>
+#include <type_traits>
+
+namespace ADS1115::util
+{
+    template <class To, class From>
+    typename std::enable_if_t<
+        sizeof(To) == sizeof(From)
+            && std::is_trivially_copyable_v<From> && std::is_trivially_copyable_v<To>,
+        To>
+    // constexpr support needs compiler magic
+    bit_cast(const From& src) noexcept
+    {
+        static_assert(
+            std::is_trivially_constructible_v<To>,
+            "This implementation additionally requires destination type to be trivially "
+            "constructible");
+
+        To dst;
+        std::memcpy(&dst, &src, sizeof(To));
+        return dst;
+    }
+} // namespace ADS1115::util
+
+#endif // ADS1115_BIT_HPP_
